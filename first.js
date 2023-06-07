023e5a2f5655b0407c49512f4fbe55d5d6ca0d23
Lasha Bagaturia <lashabagaturia1@gmail.com>
2023-06-07 13:14:41 +0400

    exercises 1-18

---
+++ first.js
@@ -0,0 +1,211 @@
+// 1 დავალება
+/*
+ console.log("Hello, World!");
+*/
+
+
+// 2 დავალება
+/*
+let myNum = 7;
+console.log(myNum);
+*/
+
+// 3 დავალება
+/*
+let firstNum = 4;
+let secondNum = 6;
+let result = firstNum + secondNum;
+console.log(result);
+*/
+
+// 4 დავალება
+/*
+let firstNum = 7;
+let secondNum = 3;
+let result = firstNum - secondNum;
+console.log(result);
+*/
+
+// 5 დავალება
+/*
+let firstNum = 4;
+let secondNum = 6;
+let result = firstNum * secondNum;
+console.log(result);
+*/
+
+// 6 დავალება
+/*
+let firstNum = 36;
+let secondNum = 6;
+let result = firstNum / secondNum;
+console.log(result);
+*/
+
+// 7 დავალება
+/*
+let firstNum = 9;
+let secondNum = 6;
+let result = firstNum % secondNum;
+console.log(result);
+*/
+
+// 8 დავალება
+/*
+let myString = "Hello, World!";
+console.log(myString.length);
+*/
+
+// 9 დავალება
+/*
+let firstString = "Hello, ";
+let secondString = "World!";
+let stringsAddition = firstString + secondString;
+console.log(stringsAddition);
+*/
+
+// 10 დავალება
+/*
+let myString = "hi, my name is lasha";
+let upperString = myString.toUpperCase();
+console.log(upperString);
+*/
+
+// 11 დავალება
+/*
+let myString = "HI, MY NAME IS LASHA";
+let lowerString = my.String.toLowerCase();
+console.log(lowerString);
+*/
+
+// 12 დავალება
+/*
+let myNum = 4;
+checkNumber(myNum);
+
+function checkNumber(x) {
+let result;
+if(x % 2 == 0) {
+    result = console.log("The number is even");
+} else {
+    result = console.log("The number is odd");
+}
+return result
+}
+*/
+
+// 13 დავალება
+/*
+let myNum = 3;
+checkNumber(myNum);
+
+function checkNumber(x) {
+let result;
+if (x > 0) {
+    result = console.log("The number is positive");
+} else if (x < 0) {
+    result = console.log("The number is negative");
+} else {
+     result = console.log("The number is zero");
+}
+return result
+}
+*/
+
+// 14 დავალება
+/*
+let firstNum = 5;
+let secondNum = 4;
+
+findMax(firstNum, secondNum);
+
+
+function findMax(a, b) {
+  let maxNum;
+  if(a == b){
+    maxNum = console.log("Numbers are equal");
+  } else if(a < b) {
+    maxNum = console.log(b);
+  } else if (a > b) {
+    maxNum = console.log(a);
+  }
+  return maxNum
+}
+*/
+
+// 15 დავალება
+/*
+let firstNum = 5;
+let secondNum = 4;
+
+findMin(firstNum, secondNum);
+
+
+function findMin(a, b) {
+  let minNum;
+  if(a == b){
+    minNum = console.log("Numbers are equal");
+  } else if(a > b) {
+    minNum = console.log(a);
+  } else if (a < b) {
+    minNum = console.log(b);
+  }
+  return minNum
+}
+*/
+
+// 16 დავალება
+/* 
+let firstNum = 5;
+let secondNum = 5;
+checkIfDivisible(firstNum, secondNum);
+
+function checkIfDivisible(a, b) {
+  let isDivisible;
+  if(a % b == 0) {
+    isDisivisble = console.log("The number is divisible by another number");
+  } else {
+    isDivisible = console.log("The number is not divisible by another number");
+  }
+  return isDivisible
+}
+*/
+
+// 17 დავალება
+/* 
+let myString = "Hello";
+
+checkIfEmptyString(myString);
+
+function checkIfEmptyString (x) {
+  let result;
+
+  if(x.length == 0) {
+    result = console.log("The string is empty");
+  }
+  else {
+    result = console.log("The string is not empty");
+  }
+  return result
+}
+*/
+
+// 18 დავალება
+/*
+let myString = "Hello, my name is Lasha";
+let substring = "name";
+checkSubstring(myString, substring);
+
+function checkSubstring (a, b) {
+  let result;
+
+  if(a.includes(b)) {
+    result = console.log("The string contains the word");
+  }
+  else {
+    result = console.log("The string does not contain the word");
+  }
+  return result
+}
+*/
+
