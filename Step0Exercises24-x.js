a5beacc512344c811b7a626ecc2f07293eff9928
Lasha Bagaturia <lashabagaturia1@gmail.com>
2023-06-08 10:54:49 +0400

    exercise 24

---
+++ Step0Exercises24-x.js
@@ -0,0 +1,21 @@
+// 24 დავალება
+/*
+let myArr = [3, 1, 5 , 6, 2, 1, 8, 4, 6];
+
+
+console.log(removeDuplicates(myArr));
+
+function removeDuplicates(x){
+    let arrContainer = [];
+    for(let i = 0; i < x.length; i++) {
+        let arrElement = x[i];
+        let arrDoesntInclude = !arrContainer.includes(arrElement);
+        
+        if(arrDoesntInclude){
+            arrContainer.push(arrElement);
+        }
+        
+    }
+    return arrContainer;
+}
+*/
